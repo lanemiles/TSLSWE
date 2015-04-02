21a06bb4ae9ac26824b9e9fac6e58a99e329e78b
Lane Miles <lanem1234@aol.com>
2015-04-02 13:51:25 -0700

    Initial

---
+++ TSLSWE/TSLSWE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TSLSWE
+//
+//  Created by Lane Miles on 4/2/15.
+//  Copyright (c) 2015 Lane Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TSLSWE/TSLSWE/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TSLSWE
+//
+//  Created by Lane Miles on 4/2/15.
+//  Copyright (c) 2015 Lane Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
